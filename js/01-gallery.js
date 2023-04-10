a268cdda7fcd691f3fc6c9a37a4494426a7cb61b
ElenaStatsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2023-04-10 17:34:38 +0300

    пробую 1 задание

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const addGallaryMarkup = document.querySelector('.gallery');
+ulRefs.innerHTML = addGallaryMarkup;
+ulRefs.addEventListener ('click', onImageClick);
+function onImageClick(evt) {
+    blockStandartAction(evt);
+    if (evt.target.nodeName !== 'IMG'){
+        return;
+    }
+    const istance = basicLightBox.create('<img src = "${evt.target.dataset.sourse"} width ="800" height ="600">');
+    instance.show();
+}
+
+ulRefs.addEventListener ('keydown', (evt) => {
+    if (evt.code === 'ESCAPE') {
+        instance.close();
+    }
+});
+function blockstandart (evt) {
+    evt.preventdefault();
+}
 
 console.log(galleryItems);
