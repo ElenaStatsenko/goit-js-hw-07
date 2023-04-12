34b4b78775e4ca5c8e297e2d01ceae699a4788d3
ElenaStatsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2023-04-12 15:48:17 +0300

    сделала 1 часть задачи

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,24 +1,61 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const addGallaryMarkup = document.querySelector('.gallery');
-ulRefs.innerHTML = addGallaryMarkup;
-ulRefs.addEventListener ('click', onImageClick);
-function onImageClick(evt) {
-    blockStandartAction(evt);
-    if (evt.target.nodeName !== 'IMG'){
-        return;
-    }
-    const istance = basicLightBox.create('<img src = "${evt.target.dataset.sourse"} width ="800" height ="600">');
-    instance.show();
-}
-
-ulRefs.addEventListener ('keydown', (evt) => {
-    if (evt.code === 'ESCAPE') {
-        instance.close();
-    }
-});
-function blockstandart (evt) {
-    evt.preventdefault();
-}
-
 console.log(galleryItems);
+
+const gallaryContainer = document.querySelector('.gallery');
+const images = galleryItems.map(galleryItem => `
+ <li class="gallery__item">
+ <a class="gallery__link" href='${galleryItem.original}'>
+//    <img class="gallery__image" src='${galleryItem.preview}'
+     data-source ='${galleryItem.original}'
+     alt='${galleryItem.description}'
+   />
+ </a>
+</li>`).join('');
+
+gallaryContainer.insertAdjacentHTML('beforeend', images);
+
+    
+
+
+// const gallaryContainer = document.querySelector('.gallery');
+// const galleryItemsContainer = createGalleryMarkup(galleryItems);
+
+// gallaryContainer.insertAdjacentHTML('beforeend', galleryItemsContainer);
+
+// function createGalleryMarkup (galleryItems) {
+//     const galleryMarkup = galleryItems.map(({preview, original, description}) => {
+//         return `<li class="gallery__item">
+//         <a class="gallery__link" href="${original}">
+//           <img class="gallery__image"src= "${preview}" alt="${description}" />
+//         </a>
+//       </li>`
+//       ;
+//       console.log(galleryMarkup[0]);
+//     }).join();
+// }
+
+
+
+// const addGallaryMarkup = document.querySelector('.gallery');
+// ulRefs.innerHTML = addGallaryMarkup;
+// ulRefs.addEventListener ('click', onImageClick);
+// function onImageClick(evt) {
+//     blockStandartAction(evt);
+//     if (evt.target.nodeName !== 'IMG'){
+//         return;
+//     }
+//     const istance = basicLightBox.create('<img src = "${evt.target.dataset.sourse"} width ="800" height ="600">');
+//     instance.show();
+// }
+
+// ulRefs.addEventListener ('keydown', (evt) => {
+//     if (evt.code === 'ESCAPE') {
+//         instance.close();
+//     }
+// });
+// function blockstandart (evt) {
+//     evt.preventdefault();
+// }
+
+// console.log(galleryItems);
