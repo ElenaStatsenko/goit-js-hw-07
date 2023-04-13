34904b410dd2cf10f616ac224fcdb1121b681c38
Olena <kovalchuk.e.u@gmail.com>
2023-04-13 22:03:40 +0300

    сделала 2 задачу

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+// Change code below this line
 console.log(galleryItems);
+
+
+const gallaryContainer = document.querySelector('.gallery'); 
+
+const images = galleryItems.map(({preview, original, description}) => `
+
+<li class="gallery__item">
+   <a class="gallery__link" href='${original}'>
+      <img class="gallery__image" src='${preview}' alt='${description}' />
+   </a>
+</li>`).join(''); 
+ 
+gallaryContainer.insertAdjacentHTML('beforeend', images); 
+
+
+const gallaryLightboxContainer = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+});
+
+   
+
+
+
+
+
+
+
+
