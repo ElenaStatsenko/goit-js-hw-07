11dd954a8764e44e81b06961e80376b97a75218e
ElenaStatsenko <118728440+ElenaStatsenko@users.noreply.github.com>
2023-04-13 17:40:53 +0300

    сделала 7.1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,59 +3,121 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 const gallaryContainer = document.querySelector('.gallery');
-const images = galleryItems.map(galleryItem => `
- <li class="gallery__item">
- <a class="gallery__link" href='${galleryItem.original}'>
-//    <img class="gallery__image" src='${galleryItem.preview}'
-     data-source ='${galleryItem.original}'
-     alt='${galleryItem.description}'
-   />
+const images = galleryItems.map(({preview, original, description}) => ` 
+<li class="gallery__item">
+<a class="gallery__link" href='${original}'>
+    <img class="gallery__image" src='${preview}'
+    data-source ='${original}'
+      alt='${description}'
+    />
  </a>
 </li>`).join('');
 
-gallaryContainer.insertAdjacentHTML('beforeend', images);
+ 
+gallaryContainer.insertAdjacentHTML('beforeend', images); 
+gallaryContainer.addEventListener('click', onImageClick);
+function onImageClick (event){
 
+  event.preventDefault();
+  if(event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+  <img src="${event.target.dataset.source}" width="800" height = "600">` 
+); 
+  instance.show();
+}
+// function onImageClick (event){
+
+//   event.preventDefault();
+
+//   if(event.target.nodeName !== "IMG") {
+//     return;
+//   }
+//   const instance = basicLightbox.create(`
+//   <img src="${event.target.dataset.sourse}" width="800" height="600">`
+//   );
+// instance.show();
+// }
+
+
+
+
+// function onImageClick (evt){ 
     
+//   // запрет стандартных действий
+//   evt.preventDefault(); 
 
-
-// const gallaryContainer = document.querySelector('.gallery');
-// const galleryItemsContainer = createGalleryMarkup(galleryItems);
-
-// gallaryContainer.insertAdjacentHTML('beforeend', galleryItemsContainer);
-
-// function createGalleryMarkup (galleryItems) {
-//     const galleryMarkup = galleryItems.map(({preview, original, description}) => {
-//         return `<li class="gallery__item">
-//         <a class="gallery__link" href="${original}">
-//           <img class="gallery__image"src= "${preview}" alt="${description}" />
-//         </a>
-//       </li>`
-//       ;
-//       console.log(galleryMarkup[0]);
-//     }).join();
+//   // проверка на картинку
+//   if(evt.target.nodeName !== "IMG") {
+//     return;
+//   }
+  
+//   // а если клацаем на картинку
+  // const instance = basicLightbox.create(`
+  //   <img src="${evt.target.dataset.source}" width="800" height = "600">` 
+  // ); 
+//   instance.show(); 
 // }
 
 
 
-// const addGallaryMarkup = document.querySelector('.gallery');
-// ulRefs.innerHTML = addGallaryMarkup;
-// ulRefs.addEventListener ('click', onImageClick);
-// function onImageClick(evt) {
-//     blockStandartAction(evt);
-//     if (evt.target.nodeName !== 'IMG'){
-//         return;
-//     }
-//     const istance = basicLightBox.create('<img src = "${evt.target.dataset.sourse"} width ="800" height ="600">');
-//     instance.show();
-// }
+// const images = galleryItems.map(galleryItem => `
+//  <li class="gallery__item">
+//  <a class="gallery__link" href='${galleryItem.original}'>
+// //    <img class="gallery__image" src='${galleryItem.preview}'
+//      data-source ='${galleryItem.original}'
+//      alt='${galleryItem.description}'
+//    />
+//  </a>
+// // </li>`).join('');
 
-// ulRefs.addEventListener ('keydown', (evt) => {
-//     if (evt.code === 'ESCAPE') {
+// gallaryContainer.insertAdjacentHTML('beforeend', images);
+// gallaryContainer.addEventListener('click', onImageClick);
+// function onImageClick (event){
+//   event.preventDefault();
+//   if(event.target.nodeName !== "IMG") {
+//     return;
+//   }
+//   const instance = basicLightbox.create(`<img src="${event.target.dataset.sourse}" width="800" height="600">`);
+// instance.show();
+
+
+
+// gallaryContainer.addEventListener('keydown', (event) => {
+//       if(event.code === 'Escape') {
 //         instance.close();
-//     }
-// });
-// function blockstandart (evt) {
-//     evt.preventdefault();
-// }
+//       }
+//     })
+  
+//   };
+
+
 
 // console.log(galleryItems);
+
+// const gallaryContainer = document.querySelector('.gallery'); 
+
+// const images = galleryItems.map(({preview, original, description}) => ` 
+//  <li class="gallery__item"> 
+//  <a class="gallery__link" href='${original}'> 
+// //    <img class="gallery__image" src='${preview}' 
+//      data-source ='${original}' 
+//      alt='${description}' 
+//    /> 
+//  </a> 
+// </li>`).join(''); 
+ 
+// gallaryContainer.insertAdjacentHTML('beforeend', images); 
+
+// gallaryContainer.addEventListener('click', onImageClick); 
+
+
+
+//     // закрытие
+//     gallaryContainer.addEventListener('keydown', (evt) => {
+//       if(evt.code === 'Escape') {
+//         instance.close()
+//       }
+//     })
+// };
